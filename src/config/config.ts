f183bb3be04e3d6ecbe9a7828440573d64e53c45
Ignacio Patino <ignaciocorreagabbi@gmail.com>
2025-12-01 16:26:34 -0300

    test conectarseRedis y conectarseDB terminados #24 #25

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,5 +1,7 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config({path: '.env.development'});
 
 export const PORT = Number(process.env.PORT) || 5003
+
+export const REDIS_URL = process.env.REDIS_URL || "redis://redis-servidor:6380"
