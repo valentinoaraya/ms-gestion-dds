2c67c3565cda5d05a73d8af567552f59b4799972
Tomas Romero <Tomasromero200310@gmail.com>
2025-12-03 17:51:09 -0300

    Mejora en test 'eliminarUniversidad'

--- tests/universidadTests/eliminarUniversidad.test.ts
+++ tests/universidadTests/eliminarUniversidad.test.ts
@@ -1,14 +1,11 @@
 import { UniversidadService } from "../../src/services/UniversidadService";
 import { instanciaUniversidad as universidad } from "../utils";
-import { PrismaClient } from "@prisma/client";
+import { prisma } from "../../src/lib/prisma";
 
 test('deberia eliminar una universidad por ID de la base de datos', async () => {
     const universidadDB = await UniversidadService.crearUniversidad(universidad);
 
     await UniversidadService.eliminarUniversidad(universidadDB.id as number);
-
-    
-    const prisma = new PrismaClient();
 
     const universidadBorrada = await prisma.universidades.findUnique({
         where: { id: universidadDB.id }
