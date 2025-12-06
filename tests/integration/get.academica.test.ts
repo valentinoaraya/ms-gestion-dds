019b865cf71ef6f9f0ea75348292bb39e026aa31
Facundo Contreras <facundocontre4375@gmail.com>
2025-12-06 03:09:47 -0300

    Arreglo tests de redis

--- tests/integration/get.academica.test.ts
+++ tests/integration/get.academica.test.ts
@@ -10,8 +10,6 @@ describe("Pruebas de integración para obtener datos académicos a partir de una
         const response = await request(`http://localhost:${PORT}`)
             .get(`/api/especialidad/${especialidad.id!}`)
 
-        await localClient.connect()
-
         const redisKey = `especialidad:${especialidad.id!}`
         const redisData = await localClient.get(redisKey)
 
