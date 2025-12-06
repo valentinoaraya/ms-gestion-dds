29176040f909dd07bd157204201d96fbdb80dc0d
Facundo Contreras <facundocontre4375@gmail.com>
2025-12-06 06:28:12 -0300

    agrego tests especialidad

---
+++ tests/units/especialidadTests/obtenerEspecialidad.test.ts
@@ -0,0 +1,35 @@
+import { Especialidad } from "../../../src/models/Especialidad";
+import { Facultad } from "../../../src/models/Facultad";
+import { EspecialidadService } from "../../../src/services/EspecialidadService";
+import { FacultadService } from "../../../src/services/FacultadService";
+import { UniversidadService } from "../../../src/services/UniversidadService";
+import { instanciaUniversidad } from "../../utils";
+
+test("debería obtener una especialidad por ID de la base de datos", async () => {
+    const universidadCreada = await UniversidadService.crearUniversidad(instanciaUniversidad);
+    const facultad = new Facultad(
+        "Facultad Regional de San Rafael",
+        "FRSR",
+        "Facultad de ingeniería",
+        "FRSR",
+        "5600",
+        "San Rafael",
+        "Urquiza 314",
+        "02604421078",
+        "Ing. Roberto D. Vilches",
+        "rvilches@frsr.utn.edu.ar",
+        universidadCreada.id!
+    );
+    const facultadCreada = await FacultadService.crearFacultad(facultad);
+    const especialidad = new Especialidad(
+        "Ingeniería en Sistemas",
+        facultadCreada.id!
+    );
+    const especialidadCreada = await EspecialidadService.crearEspecialidad(especialidad);
+    const especialidadObtenida = await EspecialidadService.obtenerEspecialidadPorId(especialidadCreada.id!);
+
+    expect(especialidadObtenida).toBeTruthy();
+    expect(especialidadObtenida?.id).toBe(especialidadCreada.id);
+    expect(especialidadObtenida?.nombre).toBe(especialidad.nombre);
+})
+
